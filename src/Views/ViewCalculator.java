f2cd0134029b8d4f4cd610ad58f7e6fbf34f648e
jackinjaxx <jackinjaxx2002@gmail.com>
2022-10-16 12:50:45 -0500

    Add iniciar()

--- src/Views/ViewCalculator.java
+++ src/Views/ViewCalculator.java
@@ -10,8 +10,15 @@ public class ViewCalculator extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 0, 0));
         setVisible(true);
-
     }
 
+    public void iniciar(){
+        for (int i = 0; i < 10; i++) {
+            JButton btn = new JButton();
+            btn.setText(String.valueOf(i));
+            btn.setBounds(10, 10, 10, 10);
+            add(btn);
+        }
+    }
 
 }
