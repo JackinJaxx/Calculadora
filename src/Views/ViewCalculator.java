86536d3ccd3f823eb98d204d6334b869ebd75938
jackinjaxx <jackinjaxx2002@gmail.com>
2022-10-16 14:14:35 -0500

    Add img icon max,min,close

--- src/Views/ViewCalculator.java
+++ src/Views/ViewCalculator.java
@@ -4,12 +4,17 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ViewCalculator extends JFrame {
+    
     public ViewCalculator(){
         setTitle("Calculator");
-        setSize(340, 530);
+        setSize(336, 540);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setBackground(Color.black);
+        getContentPane().setBackground(Color.BLACK);
+        setUndecorated(true);
+
+
+
         setVisible(true);
     }
 }
